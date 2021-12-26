ec97a2e0cf5097b53f3eee85372ae14cec90197a
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2021-12-27 02:28:20 +0530

    Personal website Link updated

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <ThemeToggle />
       <p className="text-gray-800 dark:text-red-400 text-5xl font-bold flex justify-center py-4">Flip-it</p>
       <CoinFlip/>
-      <div className="flex items-center justify-center relative py-4 dark:text-gray-200"><a href="https://github.com/VishwaGauravIn" target="_blank" rel="noreferrer" className="opacity-60 hover:opacity-95 dark:text-red-400">{'</>'} with 💛 by Vishwa Gaurav</a></div>
+      <div className="flex items-center justify-center relative py-4 dark:text-gray-200"><a href="https://itsvg.in" target="_blank" rel="noreferrer" className="opacity-60 hover:opacity-95 dark:text-red-400">{'</>'} with 💛 by Vishwa Gaurav</a></div>
     </>
   );
 }
